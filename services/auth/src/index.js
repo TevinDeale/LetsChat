cec62cc9c92b0a69044fcda8a86c2102a4ff619e
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2024-07-13 01:23:25 -0500

    resetting vue files

--- services/auth/src/index.js
+++ services/auth/src/index.js
@@ -1,7 +1,12 @@
+const { ExpressAuth } = require('@auth/express');
 const express = require('express');
 require('dotenv').config();
 const { createLoggerInstance } = require('../../../shared/utils/logger')
 
+// set up logger util
 const logger = createLoggerInstance('auth_service')
 
-const env = process.env
+const app = express()
+
+app.set('trust proxy', true)
+app.use("/auth/*", ExpressAuth({ providers: [] }))
